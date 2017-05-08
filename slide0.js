c7dd7c849559c2bf37bfc6e901162529569ed800
zsteinberg <zsteinberg@att.net>
2017-05-08 15:53:57 -0400

    Standardize point formatting: Blue for the function, deeperblue for approximations

--- slide0.js
+++ slide0.js
@@ -5,15 +5,14 @@
 present = mathbox.select("cartesian").present({
   index: 0
 });
-
-	var h = 0.5;
 	var p0 = [0.5,0.5];
 
 	function oneLinearIteration(p0, h){
 		return [p0[0] + h, p0[1] + h * f(p0[0],p0[1])]
 	}
-	var firstIteration = oneLinearIteration(p0,h)
-	var longFirstOrderGuess = oneLinearIteration(p0,1); // a terrible rough guess
+	var firstIteration = oneLinearIteration(p0,0.3)
+	var longFirstOrderGuess = oneLinearIteration(p0,0.75); // a terrible rough guess
+	var longTaylorSeriesGuess = [p0[0]+0.75,taylor_approximation(p0[0]+0.75)]
 
 	var arrowPlotH = 0.3;
 
@@ -43,8 +42,8 @@ slide1 = present.slide({from: 1, to: 50}).reveal()
       });
 
 //show first point
-present.slide()
-present.slide({to: 10}).reveal()
+present.slide() //slide 1
+present.slide({to: 10}).reveal() //slide 2
       .volume({
         expr: function (emit, x, y, i, j, t) {
 			emit(p0[0],p0[1]);
@@ -52,7 +51,7 @@ present.slide({to: 10}).reveal()
         channels: 2,
 		items: 1,
       }).point({
-		size: 10,
+		size: 15,
         zIndex: 1,
         color: deeperblue,
       }).text({
@@ -63,7 +62,7 @@ present.slide({to: 10}).reveal()
         zIndex: 1,
       });
 
-present.slide({from: 3, to: 5})
+present.slide({from: 3, to: 5}) //slide 3, which stays present until 5
 	.array({
 		    data: [
 				p0,
@@ -73,11 +72,11 @@ present.slide({from: 3, to: 5})
 			items: 1,
 		  })
 		  .point({
-			size: 10,
+			size: 15,
 		    zIndex: 1,
 		    color: deeperblue,
 		  }).text({
-		    data: ['x_0','x_f?'], //not showing x_f
+		    data: ['x_0','x_f? (first-order)'], //not showing x_f
 		  })
 		  .label({
 		    color: 'black',
@@ -97,7 +96,7 @@ present.slide({from: 3, to: 5})
         color: deeperblue,
       })
 
-present.slide().reveal()
+present.slide().reveal() // slide 4
       .interval({
 		width: 50,
         expr: function (emit, x, i, t) {
@@ -106,10 +105,10 @@ present.slide().reveal()
         channels: 2,
       }).line({
         width: 10,
-        color: deeperblue,
+        color: blue,
       })
 
-present.slide().reveal()
+present.slide({from: 5, to: 6}).reveal() // slides 5 and 6, with a taylor series
       .interval({
 		width: 50,
         expr: function (emit, x, i, t) {
@@ -120,3 +119,65 @@ present.slide().reveal()
         width: 10,
         color: deeperblue,
       })
+		//points
+		.array({
+		    data: [
+				p0,
+				longTaylorSeriesGuess,
+		    ],
+		    channels: 2,
+			items: 1,
+		  })
+		  .point({
+			size: 15,
+		    zIndex: 1,
+		    color: deeperblue,
+		  }).text({
+		    data: ['x_0','x_f? (taylor series)'], //not showing x_f
+		  })
+		  .label({
+		    color: 'black',
+		    zIndex: 1,
+		  })
+
+present.slide() //slide 7: an introduction to the runge-kutta
+present.slide() //slide 8: definition
+present.slide() //slide 9: definition, cont.
+
+
+var RungeKuttaH = 0.75;
+
+present.slide().reveal() //slide 10: time to shine!
+	.array({
+		    data: [
+				p0,
+				oneLinearIteration(p0,RungeKuttaH/2),
+		    ],
+		    channels: 2,
+			items: 1,
+		  })
+		  .point({
+			size: 15,
+		    zIndex: 1,
+		    color: deeperblue,
+		  }).text({
+		    data: ['','k_1'], //not showing x_f
+		  })
+		  .label({
+		    color: 'black',
+		    zIndex: 1,
+		  })
+	 .array({
+        data: [
+			p0,
+			oneLinearIteration(p0,RungeKuttaH/2),
+        ],
+        channels: 2,
+		items: 2,
+      })
+		.vector({
+        end: true,
+        width: 5,
+        color: deeperblue,
+      })
+
